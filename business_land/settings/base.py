4dd32e1650b935b4bde4d6f2a85f4c78f18c1a4c
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2020-08-30 22:52:33 +0600

    testing branch

--- business_land/settings/base.py
+++ business_land/settings/base.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a$9u4((gr%(p7@o#sv1t^=o=a%7bs@odb-7qkb4($aso)xc6uw'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
