6b7d6511131dd15a2c5ed9889b2c989f40d83e9d
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2020-10-18 03:47:43 +0600

    multi search added

---
+++ venv/lib/python3.6/site-packages/django_structlog/__init__.py
@@ -0,0 +1,10 @@
+""" ``django-structlog`` is a structured logging integration for ``Django`` project using ``structlog``.
+"""
+
+
+default_app_config = "django_structlog.apps.DjangoStructLogConfig"
+name = "django_structlog"
+
+VERSION = (1, 4, 1)
+
+__version__ = ".".join(str(v) for v in VERSION)
---
+++ venv/lib/python3.6/site-packages/django_structlog/celery/steps.py
@@ -0,0 +1,39 @@
+from celery import bootsteps
+
+from . import receivers
+
+
+class DjangoStructLogInitStep(bootsteps.Step):
+    """ ``celery`` worker boot step to initialize ``django_structlog``.
+
+    >>> from celery import Celery
+    >>> from django_structlog.celery.steps import DjangoStructLogInitStep
+    >>>
+    >>> app = Celery("django_structlog_demo_project")
+    >>> app.steps['worker'].add(DjangoStructLogInitStep)
+
+    """
+
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        from celery.signals import (
+            before_task_publish,
+            after_task_publish,
+            task_prerun,
+            task_retry,
+            task_success,
+            task_failure,
+            task_revoked,
+            task_unknown,
+            task_rejected,
+        )
+
+        before_task_publish.connect(receivers.receiver_before_task_publish)
+        after_task_publish.connect(receivers.receiver_after_task_publish)
+        task_prerun.connect(receivers.receiver_task_pre_run)
+        task_retry.connect(receivers.receiver_task_retry)
+        task_success.connect(receivers.receiver_task_success)
+        task_failure.connect(receivers.receiver_task_failure)
+        task_revoked.connect(receivers.receiver_task_revoked)
+        task_unknown.connect(receivers.receiver_task_unknown)
+        task_rejected.connect(receivers.receiver_task_rejected)
